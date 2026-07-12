3d69e1ed0c22db098f36154784a8b68f19472b6e
Carlos <carlosvtm01@icloud.com>
2026-07-12 19:55:53 +0100

    feat: add user model

---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,31 @@
+import { InferSchemaType, model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export type User = InferSchemaType<typeof userSchema>;
+export const UserModel = model<User>('User', userSchema);
